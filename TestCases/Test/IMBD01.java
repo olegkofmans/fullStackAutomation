3cb27828ff0b65d36aae1c018f3ea478493db9a2
kofmans <olegkofmans@gmail.com>
2019-07-15 11:39:53 +0300

    update

--- TestCases/Test/IMBD01.java
+++ TestCases/Test/IMBD01.java
@@ -13,9 +13,8 @@ public class IMBD01 extends commonOps{
 
 	@Test
 	public void test01_searchValue() throws IOException, ParserConfigurationException, SAXException, FindFailed {
-		
 		web_search.go("The Lion King");
 		verify.textInElement(imbdResults.resulField, "The Lion King (2019)");
-		verify.image("./imageRepository/logo.png");
+		verify.image(System.getProperty("user.dir") +"./imageRepository/logo.png");
 	}
 }
